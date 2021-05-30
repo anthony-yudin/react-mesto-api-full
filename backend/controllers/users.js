89158917e2533014006b6978d9d3a2604f5d1625
Anton <yudin-1337@yandex.ru>
2021-05-30 21:28:53 +0500

    Синхронизация бэка и фронта

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -18,10 +18,9 @@ exports.login = (req, res, next) => {
         { _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' },
       );
 
-      const test = NODE_ENV === 'production2' ? JWT_SECRET : 'some-secret-key';
-
       res.send({
-        test,
+        email,
+        token,
       });
     })
     .catch((err) => {
